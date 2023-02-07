043aa29a962b332ec5bac1338ee329de691f5b91
elceren <114217376+elceren@users.noreply.github.com>
2023-02-07 20:46:12 +0300

    Add files via upload

--- Cipher.java
+++ Cipher.java
@@ -15,22 +15,28 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
     }
 
     public String decrypt(String inputString) {
-        
-        // output string will be collected in this variable, one char at a time
+    // output string will be collected in this variable, one char at a time
+
         String outputString = "";
-        
+
+        for (int i = 0; i < inputString.length(); i++) 
+        {
+            // append the encrypted version of the char to the output string
+            outputString += replaceChar(inputString.charAt(i), false);
+        }
+
         replaceChar('a',true);
         
         return outputString;
-    }
-
+    }       
+   
     // replaces the given input char based on the given isEncrypt variable
     // if isEncrypt == true -> original to encrypted
     // if isEncrypt == false -> encrypted to original
@@ -42,7 +48,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
